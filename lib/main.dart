5b7bb04632997a8b1a9d80a41f4b70081b8baefc
sinv207 <sinv207@gmail.com>
2020-08-13 22:00:49 +0700

    Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat UI',
+      title: 'Flutter Chat App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.red,
